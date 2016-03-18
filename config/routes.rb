a1c43805a594bb4e1c53893d1c163a63d9df39fb
Jonathan Li <icsshtml@gmail.com>
2016-03-18 21:05:40 +0000

    completed lesson 17

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,14 @@ Rails.application.routes.draw do
 
   resources :courses, only: [:index, :show]
   namespace :instructor do
-    resources :courses, only: [:new, :create, :show] do
-      resources :sections, only: [:new, :create]
+    resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
     end
-  end
+    resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:new, :create] 
+      end
+    end
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
