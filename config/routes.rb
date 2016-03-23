ec5e071ce809cfb563edfb2ebce11ed9fecd3fa1
Jonathan Li <icsshtml@gmail.com>
2016-03-23 20:30:32 +0000

    completed lesson 33

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resource :dashboard, only: [:show]
   root 'static_pages#index'
 
   resources :courses, only: [:index, :show] do
@@ -8,8 +9,8 @@ Rails.application.routes.draw do
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :lessons, only: [:update]
-    resources :section, only: [:update]
-    resources :sections, only: [] do
+    resources :sections, only: [:update] do
+    # resources :sections, only: [] do
       resources :lessons, only: [:new, :create]
     end
     resources :courses, only: [:new, :create, :show] do
